9c7cfd9b0e0e38c9e351d1e0912abc54f97c55f9
Mohammed Sami Ahmed <mohammedsamiahmed@Mohammeds-MacBook-Pro.local>
2024-10-13 01:01:36 -0400

    pushing all my updates files

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import './providers/game_state.dart';
+import './widgets/card_grid.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => GameState(),
+      child: MaterialApp(
+        title: 'Card Matching Game',
+        home: Scaffold(
+          appBar: AppBar(
+            title: Text('Card Matching Game'),
+          ),
+          body: CardGrid(),
+        ),
+      ),
+    );
+  }
+}
